a982e67b9f3b5958f758d72a1f11ddf63865bf5b
Ben Krig <benjaminjkrig@gmail.com>
2017-04-06 20:26:21 -0700

    Update facial_recognition_fisher.cpp

--- facial_recognition_fisher.cpp
+++ facial_recognition_fisher.cpp
@@ -1,3 +1,26 @@
+/*
+My output for running this is:
+First Label: 1
+First Confidence: 14.203817
+second Label: 0
+second Confidence: 0.000000
+third Label: 0
+third Confidence: 910.301280
+Program ended with exit code: 0
+
+So, first image is analyzed correctly. That was an image of my face and the trainers were labeled as 1.
+
+Second image was analyzed correctly also, that was a picture of harrison ford, and trainers were labeled at 0.
+
+Third image was analyzed incorrectly, but we can see that the confidence (or distance) was really high.
+
+That's where model -> setThreshold(DOUBLE); comes into play. You'd play around with that value until it was rejecting
+the third label.
+
+*/
+
+
+
 #include <iostream>
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -16,14 +39,15 @@ int main()
     cv::Size size(250,250);
     
 
-    //These are the images we will be identifying
+    //These are the images we will be identifying, 1 image of myself, 1 image of harrison ford, 1 image of hillary clinton
     cv::Mat ben;
     cv::Mat harrison;
-    cv::Mat obama;
+    cv::Mat hillary;
     resize(cv::imread("/Users/benkrig/Desktop/ben9.jpg", CV_LOAD_IMAGE_GRAYSCALE), ben, size, 1.0, 1.0, cv::INTER_CUBIC);
     resize(cv::imread("/Users/benkrig/wwwbenkrigcom/public/blogs/ford/0.jpeg", CV_LOAD_IMAGE_GRAYSCALE), harrison, size, 1.0, 1.0, cv::INTER_CUBIC);
     resize(cv::imread("/Users/benkrig/Desktop/hillary.jpg", CV_LOAD_IMAGE_GRAYSCALE), obama, size, 1.0, 1.0, cv::INTER_CUBIC);
     
+    //These are ALL different images of Harrison Ford's face.
     images.push_back(cv::imread("/Users/benkrig/wwwbenkrigcom/public/blogs/ford/0.jpeg", CV_LOAD_IMAGE_GRAYSCALE));
     labels.push_back(0);
     images.push_back(cv::imread("/Users/benkrig/wwwbenkrigcom/public/blogs/ford/1.jpeg", CV_LOAD_IMAGE_GRAYSCALE));
@@ -37,6 +61,7 @@ int main()
     images.push_back(cv::imread("/Users/benkrig/wwwbenkrigcom/public/blogs/ford/5.jpeg", CV_LOAD_IMAGE_GRAYSCALE));
     labels.push_back(0);
     
+    //These are ALL different images of my face
     images.push_back(cv::imread("/Users/benkrig/Desktop/ben1.jpg", CV_LOAD_IMAGE_GRAYSCALE));
     labels.push_back(1);
     images.push_back(cv::imread("/Users/benkrig/Desktop/ben2.jpg", CV_LOAD_IMAGE_GRAYSCALE));
@@ -94,3 +119,5 @@ int main()
     
     return 0;
 }
+
+
